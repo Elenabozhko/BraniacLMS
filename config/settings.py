7bb51b4f559a85fdff8457d1ef200ab0a45791c8
Elenabozhko <95896538+Elenabozhko@users.noreply.github.com>
2023-03-02 18:42:00 +0400

    Lesson6 (#7)
    * lesson_6 Link to admin
    
    * lesson_6 Customizing admin panel
    
    * lesson_6 New user register form
    
    * lesson_6 User edit form
    
    * lesson_6 News CRUD and permissions
    
    * lesson_6 Feedbacks with AJAX

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "social_django",
     "mainapp",
     "authapp",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -178,3 +179,5 @@ STATICFILES_DIRS = [
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR / "media"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
